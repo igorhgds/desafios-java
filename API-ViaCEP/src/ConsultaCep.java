58e1c8b6590b698525e8bae1a84e8da56aa07c4e
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-03-06 19:12:00 -0300

    criando o gerador de arquivo Json

--- API-ViaCEP/src/ConsultaCep.java
+++ API-ViaCEP/src/ConsultaCep.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -10,21 +9,17 @@ public class ConsultaCep {
     public Endereco buscaEndereco(String cep) {
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient
+            HttpResponse<String> response = HttpClient
                     .newHttpClient()
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Não consegui obter o endereço a partir desse CEP.");
         }
-
-        return new Gson().fromJson(response.body(), Endereco.class);
-
     }
 }
